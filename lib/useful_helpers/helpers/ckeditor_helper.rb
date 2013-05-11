0e988f9b07a4ca14af93204774b35665a7708e2d
Andrey <railscode@gmail.com>
2013-05-12 01:47:47 +0700

    ckeditor fix

--- lib/useful_helpers/helpers/ckeditor_helper.rb
+++ lib/useful_helpers/helpers/ckeditor_helper.rb
@@ -1,12 +1,6 @@
 # coding: utf-8
 module CkeditorHelper
   def ckeditor
-    content_for :javascripts do
-      javascript_include_tag("ckeditor/init")
-    end
-
-    content_for :stylesheets do
-      stylesheet_link_tag("ckeditor")
-    end
+    render "shared/ckeditor"
   end
 end
