c6a612044a29249c8fb654f7d4656e64a47f9820
Andrey <railscode@gmail.com>
2013-07-27 17:05:50 +0700

    current

--- lib/useful_helpers/helpers/date_time_helper.rb
+++ lib/useful_helpers/helpers/date_time_helper.rb
@@ -2,13 +2,15 @@
 module UsefulHelpers
   module Helpers
     module DateTimeHelper
-	    def localize_date(date, format=:long)
-	      begin
-  		    I18n.localize(date, :format => format)
-		    rescue
-		      ""
-		    end
-	    end
+      def localize_date(date, format=:long)
+        I18n.localize(date, :format => format) rescue ""
+      end
+
+      ["long", "short"].each do |date_format|
+        define_method "#{date_format}date" do |date|
+          I18n.l(date, :format => date_format.to_sym) rescue ""
+        end
+      end
     end
   end
 end
