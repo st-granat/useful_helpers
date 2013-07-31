2794eab6afe8e41a76acb11a38749ad18ba79d4f
Andrey <railscode@gmail.com>
2013-08-01 00:01:19 +0700

    fix buttons helper

--- lib/useful_helpers/helpers/buttons_helper.rb
+++ lib/useful_helpers/helpers/buttons_helper.rb
@@ -1,8 +1,12 @@
 # coding: utf-8
-module ButtonsHelper
-  def buttons(&block)
-    content_for :buttons do
-      content_tag :div, :class => "buttons", &block
+module UsefulHelpers
+  module Helpers
+    module ButtonsHelper
+      def buttons(&block)
+        content_for :buttons do
+          content_tag :div, :class => "buttons", &block
+        end
+      end
     end
   end
 end
